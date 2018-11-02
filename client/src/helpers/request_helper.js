ceabebbd917452be38318a064e593ab3fb01c928
Michael Griffin <michaelgriffin121@gmail.com>
2018-11-02 15:28:51 +0000

    added helper files front end

---
+++ client/src/helpers/request_helper.js
@@ -0,0 +1,34 @@
+const RequestHelper = function (url) {
+  this.url = url;
+};
+
+RequestHelper.prototype.get = function () {
+  return fetch(this.url)
+    .then((response) => response.json());
+};
+
+RequestHelper.prototype.post = function (payload) {
+  return fetch(this.url, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((response) => response.json());
+};
+
+RequestHelper.prototype.delete = function (id) {
+  return fetch(`${this.url}/${id}`, {
+    method: 'DELETE'
+  })
+    .then((response) => response.json());
+};
+
+RequestHelper.prototype.update = function (id) {
+  return fetch(`${this.url}/${id}`, {
+    method: 'UPDATE'
+  })
+    .then((response) => response.json());
+};
+
+
+module.exports = RequestHelper;
