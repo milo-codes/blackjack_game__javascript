84729160542081b4cefd4a4d9a472906710a410f
Michael Griffin <michaelgriffin121@gmail.com>
2018-11-02 16:02:16 +0000

    added pseudo code for app.js

---
+++ client/src/app.js
@@ -0,0 +1,21 @@
+// require the relevant files n stuff
+
+// listener for dom content loaded
+
+// document query selector for container - player
+// create new instance of player view  (container)
+//call bind events on player view
+
+// document query selector for container - dealer
+// create new instance of dealer view  (container)
+//call bind events on dealer view
+
+// document query selector for container - result
+// create new instance of result view  (container)
+//call bind events on result view
+
+
+//url for model to use
+// new instance of model
+// call model bind events
+// call model get data
