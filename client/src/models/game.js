a01c9c07e383fb4e6f7ae856857ef4ffc787fca5
Genna Walsh <gennawalsh@gmail.com>
2018-11-04 16:42:32 +0000

    draw cards functionality in game model

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -4,6 +4,7 @@ const Game = function () {
   // this.remainingCards = 0
   this.newDeckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6';
   // second url for draw?
+
   this.requestDeck = new RequestHelper(this.newDeckUrl);
 }
 
@@ -16,7 +17,19 @@ Game.prototype.getShuffledDeck = function () {
   this.requestDeck.get()
     .then((shuffledDeck) => {
       this.deckId = shuffledDeck.deck_id;
-      console.log(this.deckId);
+      this.newCardsUrl = `https://deckofcardsapi.com/api/deck/${ this.deckId }/draw/?count=2`;
+    });
+  // this.newCardsUrl = `https://deckofcardsapi.com/api/deck/${ this.deckId }/draw/?count=2`;
+  // this.drawTwoCards();
+  // console.log(this.deckId);
+  // console.log(this.newCardsUrl);
+};
+
+Game.prototype.drawTwoCards = function () {
+  this.requestCards = new RequestHelper(this.newCardsUrl);
+  this.requestCards.get()
+    .then((drawnCards) => {
+      console.log(drawnCards.cards);
     })
 };
 
