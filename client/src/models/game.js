d3733406045f1df6dc9ef558022d8f5cbb0570d3
Michael Griffin <michaelgriffin121@gmail.com>
2018-11-05 10:55:07 +0000

    conversion working starting get result method

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -2,12 +2,7 @@ const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require("../helpers/pub_sub.js");
 
 const Game = function () {
-  this.playerCards = [];
-  this.dealerCards = [];
-  // this.remainingCards = 0
   this.newDeckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6';
-  // second url for draw?
-
   this.requestDeck = new RequestHelper(this.newDeckUrl);
 }
 
@@ -19,83 +14,116 @@ Game.prototype.bindEvents = function () {
 Game.prototype.getShuffledDeck = function () {
   this.requestDeck.get()
     .then((shuffledDeck) => {
-      this.deckId = shuffledDeck.deck_id;
-      this.newCardsUrl = `https://deckofcardsapi.com/api/deck/${ this.deckId }/draw/?count=2`;
+      this.newCardsUrl = `https://deckofcardsapi.com/api/deck/${ shuffledDeck.deck_id }/draw/?count=2`;
+      return shuffledDeck.deck_id;
     })
-    .then(() => {
-      this.dealPlayerTwoCards()
+    .then((deckId) => {
+      this.dealCards(deckId);
     })
-    .then(() => {
-      this.dealDealerTwoCards()
-    })
-    .then(() => {
-      this.getResult()
-      // get both totals and calc
-    });
 };
 
-Game.prototype.dealPlayerTwoCards = function () {
+Game.prototype.dealCards = function (deckId) {
+  const roundObject = {};
   this.requestCards = new RequestHelper(this.newCardsUrl);
   this.requestCards.get()
     .then((drawnCards) => {
-      this.playerCards = drawnCards.cards;
-      PubSub.publish("Game:player-cards-ready", this.playerCards);
-      return this.playerCards;
+      this.convert(drawnCards.cards)
+      console.log(drawnCards.cards);
+      roundObject.playerCards = drawnCards.cards;
+      PubSub.publish("Game:player-cards-ready", roundObject.playerCards);
     })
     .then(() => {
-      this.playerCards.forEach((cardObject) => {
-        // decide where we want the values re-assigned:
-        if (cardObject.value === "JACK") {
-          cardObject.value = "10";
-        }
-        else if (cardObject.value === "QUEEN") {
-          cardObject.value = "10";
-        }
-        else if (cardObject.value === "KING") {
-          cardObject.value = "10";
-        }
-        else if (cardObject.value === "ACE") {
-          cardObject.value = "11";
-        }
-      });
-      return this.playerCards;
+      this.requestCards.get()
+        .then((drawnCards) => {
+          this.convert(drawnCards.cards)
+          roundObject.dealerCards = drawnCards.cards;
+          PubSub.publish("Game:dealer-cards-ready", roundObject.dealerCards);
+          this.getResult(roundObject);
+        });
     })
 };
 
-Game.prototype.dealDealerTwoCards = function () {
-  this.requestCards = new RequestHelper(this.newCardsUrl);
-  this.requestCards.get()
-    .then((drawnCards) => {
-      this.dealerCards = drawnCards.cards;
-      PubSub.publish("Game:dealer-cards-ready", this.dealerCards);
-    })
-    .then(() => {
-      this.dealerCards.forEach((cardObject) => {
-        if (cardObject.value === "JACK") {
-          cardObject.value = "10";
-        }
-        else if (cardObject.value === "QUEEN") {
-          cardObject.value = "10";
-        }
-        else if (cardObject.value === "KING") {
-          cardObject.value = "10";
-        }
-        else if (cardObject.value === "ACE") {
-          cardObject.value = "11";
-        }
-      });
-    })
+Game.prototype.convert = function (drawnCards) {
+  drawnCards.forEach((cardObject) => {
+    // decide where we want the values re-assigned:
+    if (cardObject.value === "JACK") {
+      cardObject.value = "10";
+    }
+    else if (cardObject.value === "QUEEN") {
+      cardObject.value = "10";
+    }
+    else if (cardObject.value === "KING") {
+      cardObject.value = "10";
+    }
+    else if (cardObject.value === "ACE") {
+      cardObject.value = "11";
+    }
+  });
 };
 
-Game.prototype.getResult = function () {
-  this.getPlayerTotal();
+// Game.prototype.dealPlayerTwoCards = function () {
+//   this.requestCards = new RequestHelper(this.newCardsUrl);
+//   this.requestCards.get()
+//     .then((drawnCards) => {
+//       this.playerCards = drawnCards.cards;
+//       PubSub.publish("Game:player-cards-ready", this.playerCards);
+//       return this.playerCards;
+//     })
+//     .then(() => {
+//       this.playerCards.forEach((cardObject) => {
+//         // decide where we want the values re-assigned:
+//         if (cardObject.value === "JACK") {
+//           cardObject.value = "10";
+//         }
+//         else if (cardObject.value === "QUEEN") {
+//           cardObject.value = "10";
+//         }
+//         else if (cardObject.value === "KING") {
+//           cardObject.value = "10";
+//         }
+//         else if (cardObject.value === "ACE") {
+//           cardObject.value = "11";
+//         }
+//       });
+//       return this.playerCards;
+//     })
+// };
+//
+// Game.prototype.dealDealerTwoCards = function () {
+//   this.requestCards = new RequestHelper(this.newCardsUrl);
+//   this.requestCards.get()
+//     .then((drawnCards) => {
+//       this.dealerCards = drawnCards.cards;
+//       PubSub.publish("Game:dealer-cards-ready", this.dealerCards);
+//     })
+//     .then(() => {
+//       this.dealerCards.forEach((cardObject) => {
+//         if (cardObject.value === "JACK") {
+//           cardObject.value = "10";
+//         }
+//         else if (cardObject.value === "QUEEN") {
+//           cardObject.value = "10";
+//         }
+//         else if (cardObject.value === "KING") {
+//           cardObject.value = "10";
+//         }
+//         else if (cardObject.value === "ACE") {
+//           cardObject.value = "11";
+//         }
+//       });
+//     })
+// };
+
+Game.prototype.getResult = function (roundObject) {
+  console.log(roundObject);
+  // this.getPlayerTotal();
   // this.getDealerTotal();
   // TODO calc who wins
 };
 
-Game.prototype.getPlayerTotal = function () {
-  console.log(this.playerCards); // --> CURRENTLY EMPTY ARRAY - check order of functions being called
-
-};
+// Game.prototype.getPlayerTotal = function () {
+//   console.log(this.playerCards); // --> CURRENTLY EMPTY ARRAY - check order of functions being called
+//
+// };
 
 module.exports = Game;
