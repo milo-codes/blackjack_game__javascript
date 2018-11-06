8b49515441e790704d0a6750a08f082fdcb97284
Genna Walsh <gennawalsh@gmail.com>
2018-11-06 11:09:07 +0000

    hide dealer's first card

--- client/src/views/dealer_view.js
+++ client/src/views/dealer_view.js
@@ -15,11 +15,19 @@ DealerView.prototype.bindEvents = function () {
 
 DealerView.prototype.render = function (cards) {
   this.container.innerHTML = "";
-  
-  cards.forEach( (card) => {
-  const cardImage = document.createElement('img');
-  cardImage.src = card.image;
-  this.container.appendChild(cardImage);
+
+  cards.forEach( (card, index) => {
+    // hide first dealer card:
+    if (index == 0) {
+      const hiddenCardImage = document.createElement('img');
+      hiddenCardImage.src = "/css/hidden_card.png";
+      this.container.appendChild(hiddenCardImage);
+    }
+    else {
+      const cardImage = document.createElement('img');
+      cardImage.src = card.image;
+      this.container.appendChild(cardImage);
+    }
 })};
 
 
